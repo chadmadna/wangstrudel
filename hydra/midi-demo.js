127477b0782361bbf84a7c389b2f6b735ff7d261
irsyad nabil <irsyad.nabil@littlelives.com>
2026-05-31 12:22:18 +0700

    update: midi demo

--- hydra/midi-demo.js
+++ hydra/midi-demo.js
@@ -7,13 +7,13 @@ await midi.start({
 midi.show()
 
 let trig = note('*', 0)
-	.adsr(0, 500, .6, 500)
+	.adsr(100, Ï500, .6, 500)
 let knob1 = cc(21).smooth(0.5)
 
 let fade = solid(trig, trig, trig)
 let mask1 = osc(trig.range(5, 30), 0, 0)
 	.kaleid(99)
-	.modulateScale(osc(trig.range(0, 20), 0, 0)
+	.modulateScale(osc(trig.range(5, 40), 0, 0.3)
 		.kaleid(4), 1)
 	.diff(fade)
 	.luma()
@@ -25,10 +25,10 @@ let mask2 = noise(trig.range(0, 20), 0)
 	.diff(fade)
 	.luma()
 
-let scene1 = () => osc(100, 10, .4).mask(mask1)
+let scene1 = () => osc(100, 10, .4).mask(mask1).mult(fade)
 	.out(o1)
 
-let scene2 = () => osc(100, 10, .4).mask(mask2)
+let scene2 = () => osc(100, 10, .4).mask(mask2).mult(fade)
 	.out(o1)
 
 let scene3 = () => src(s1)
@@ -44,12 +44,19 @@ let scene4 = () => src(s3)
 	.out(o1)
 
 let bg = src(s2)
-	.modulate(src(s2), .1)
-	.scrollY(() => time % 1000 * 0.1)
-	.modulate(noise(1, .01), .05
-             )
+	.modulate(src(s2), knob1.range(0.1, 1))
+	.scrollY(knob1.value(v => (time % 1000 * v + 0.2)))
+	.modulate(noise(1, 0.01), knob1.range(0.05, 0.2))
 	.out(o2)
 
+// midi in from controller
+midi.input(0).channel(9)
+	.onNote('*', ({ note, velocity, channel }) => {
+		if ([36, 57, 41, 43].includes(note)) { scene1(); return; }
+  		if ([38, 46, 49].includes(note)) { scene2(); return; }
+	})
+
+// midi in from DAW
 midi.input(2).channel(0)
 	.onNote('*', ({ note, velocity, channel }) => {
 		if ([36, 57, 41, 43].includes(note)) { scene1(); return; }
@@ -62,6 +69,7 @@ midi.input(0).channel(9).onNote(55, () => scene4())
 src(o2)
 	.diff(src(o1).scale(1, 1, window.innerWidth / window.innerHeight))
 	.out(o0)
+
 
 s1.initVideo("https://media0.giphy.com/media/3ohs7O2afIz1a8bWPm/giphy.mp4")
 s2.initImage("https://upload.wikimedia.org/wikipedia/commons/b/bf/Hieronymus_Bosch_-_Triptych_of_Garden_of_Earthly_Delights_%28detail%29_-_WGA2526.jpg")
